2f546a8732ef139edb374b0402bded035dfd5102
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2026-04-29 11:05:01 +0200

    Add in PYTHONPATH and submit slurm files

---
+++ pythonpath.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+jem_repo_path=/home/tienyiao/projects_local/project_jax-esm/jem_repo
+export PYTHONPATH=$jem_repo_path/jax-gcm-fixed-solar-radiation:$jem_repo_path/jax-esm/worktree/som-equilibrium
